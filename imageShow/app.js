aff749fbbb78823561de485e09ff00717924ca2c
Anbalagan Amirthalingam <anbamirt@publicisgroupe.net>
2021-07-14 10:50:24 +0200

    first commit

---
+++ imageShow/app.js
@@ -0,0 +1,6 @@
+let fullimgBox=document.getElementById('full-imgBox');
+let fullImg=document.getElementById('fullImg')
+function open(){
+    fullimgBox.style.display='flex';
+    console.log('clicked')
+}
